1e0b3c3d9855f8ce1c9d22569e447d7c75ce5dce
alexandregagne01 <alexandre.gagne.01@gmail.com>
2023-11-15 15:25:56 -0500

    Petit changement chemin

--- vamo/dentaire/standalone.js
+++ vamo/dentaire/standalone.js
@@ -1,4 +1,4 @@
-import { init } from '../vamo/dentaire/config.js'; init(
+import { init } from '../dentaire/config.js'; init(
 
     {fichier:"test/scene.gltf","titre":"Bouche humaine détaillée","attribution":'\"Human mouth detailed\" (https://sketchfab.com/3d-models/human-mouth-detailed-522eda0ec0e3413a914b1b298a791320) by Mince (https://sketchfab.com/Mince) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)'}
 
